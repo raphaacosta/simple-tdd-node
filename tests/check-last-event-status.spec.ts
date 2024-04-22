da1afa2a8d6ce02184107f4619a46be6263e628e
Raphael Costa <rafa.costa0@hotmail.com>
2024-04-22 05:21:41 -0300

    chore: ensure checkLastEventStatus returns inReview when now is equal to review time

--- tests/check-last-event-status.spec.ts
+++ tests/check-last-event-status.spec.ts
@@ -123,4 +123,18 @@ describe('CheckLastEventStatus', () => {
 
     expect(eventStatus.status).toBe('inReview');
   });
+
+  it('should return status inReview when event date is equal to review time', async () => {
+    const reviewDurationInHours = 1
+    const reviewDurationInMs = reviewDurationInHours * 60 * 60 * 1000
+    const { sut, loadLastEventRepository } = makeSut();
+    loadLastEventRepository.output = {
+      endDate: new Date(new Date().getTime() - reviewDurationInMs),
+      reviewDurationInHours
+    }
+
+    const eventStatus = await sut.perform({ groupId })
+
+    expect(eventStatus.status).toBe('inReview');
+  });
 });
