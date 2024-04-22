778d0eeb5c0eeef63acc9471de9ac9e74a8f5d9e
Raphael Costa <rafa.costa0@hotmail.com>
2024-04-22 03:55:01 -0300

    feat: ensure CheckLastEventStatus calls loadLastEventRepo with correct groupId

--- tests/check-last-event-status.spec.ts
+++ tests/check-last-event-status.spec.ts
@@ -1,17 +1,27 @@
 class CheckLastEventStatus {
-  async perform(groupId: string): Promise<void> {
+  constructor(private readonly loadLastEventRepository: LoadLastEventRepository) { }
 
+  async perform(groupId: string): Promise<void> {
+    await this.loadLastEventRepository.loadLastEvent(groupId);
   }
 }
 
-class LoadLastEventRepository {
+interface LoadLastEventRepository {
+  loadLastEvent: (groupId: string) => Promise<void>;
+}
+
+class LoadLastEventRepositoryMock implements LoadLastEventRepository {
   groupId?: string
+
+  async loadLastEvent(groupId: string): Promise<void> {
+    this.groupId = groupId;
+  }
 }
 
 describe('CheckLastEventStatus', () => {
   it('should get last event data', async () => {
-    const loadLastEventRepository = new LoadLastEventRepository()
-    const checkLastEventStatus = new CheckLastEventStatus();
+    const loadLastEventRepository = new LoadLastEventRepositoryMock()
+    const checkLastEventStatus = new CheckLastEventStatus(loadLastEventRepository);
 
     await checkLastEventStatus.perform('any_group_id')
 
