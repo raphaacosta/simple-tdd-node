e5039ddc7b95826a9cdfb0a960080262d7c3133a
Raphael Costa <rafa.costa0@hotmail.com>
2024-04-22 04:12:47 -0300

    feat: ensure CheckLastEventStatus retruns status done when group has no event clear

--- tests/check-last-event-status.spec.ts
+++ tests/check-last-event-status.spec.ts
@@ -1,33 +1,48 @@
 class CheckLastEventStatus {
   constructor(private readonly loadLastEventRepository: LoadLastEventRepository) { }
 
-  async perform(groupId: string): Promise<void> {
+  async perform(groupId: string): Promise<string> {
     await this.loadLastEventRepository.loadLastEvent(groupId);
+
+    return 'done';
   }
 }
 
 interface LoadLastEventRepository {
-  loadLastEvent: (groupId: string) => Promise<void>;
+  loadLastEvent: (groupId: string) => Promise<undefined>;
 }
 
-class LoadLastEventRepositoryMock implements LoadLastEventRepository {
+class LoadLastEventRepositorySpy implements LoadLastEventRepository {
   groupId?: string
   callsCount = 0
+  output: undefined
 
-  async loadLastEvent(groupId: string): Promise<void> {
+  async loadLastEvent(groupId: string): Promise<undefined> {
     this.groupId = groupId;
     this.callsCount++;
+    return this.output;
   }
 }
 
 describe('CheckLastEventStatus', () => {
   it('should get last event data', async () => {
-    const loadLastEventRepository = new LoadLastEventRepositoryMock()
+    const loadLastEventRepository = new LoadLastEventRepositorySpy()
     const systemUnderTest = new CheckLastEventStatus(loadLastEventRepository);
 
     await systemUnderTest.perform('any_group_id')
 
     expect(loadLastEventRepository.groupId).toBe('any_group_id');
     expect(loadLastEventRepository.callsCount).toBe(1);
+  });
+
+  it('should return status done when group has no event', async () => {
+    const loadLastEventRepository = new LoadLastEventRepositorySpy()
+    loadLastEventRepository.output = undefined
+
+    const systemUnderTest = new CheckLastEventStatus(loadLastEventRepository);
+
+    const status = await systemUnderTest.perform('any_group_id')
+
+    expect(status).toBe('done');
   });
 });
